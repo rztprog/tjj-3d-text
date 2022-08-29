1b4a7f1374009ecf6262f579db19219e42b3d9a1
Rzt Prog <rztprog@outlook.fr>
2022-08-29 18:33:20 +0200

    Add gsap

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import gsap from 'gsap'
 
 /**
  * Base
@@ -28,7 +29,6 @@ const material = new THREE.MeshMatcapMaterial({ matcap: matCapTexture });
 /**
  * Fonts
  */
-
 const fontLoader = new FontLoader();
 
 fontLoader.load(
@@ -50,102 +50,74 @@ fontLoader.load(
             }
         )
 
-        // textGeometry.computeBoundingBox()
-        // textGeometry.translate(
-        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-        // )
-        // console.log(textGeometry.boundingBox)
-
-        textGeometry.center()
+        // Add text to scene
         const text = new THREE.Mesh(textGeometry, material)
-        
-        material.wireframe = false
-        const rotationSpeed = {speed: 0}
-        panel.add(material, 'wireframe', true, false).name("Material Wireframe")
-        // panel.add(text.rotation, 'y').min(0).max(Math.PI * 2).step(0.01).name("Rotation")
         scene.add(text)
 
+        // Variables
+        textGeometry.center()
+        material.wireframe = false
+        const rotationSpeed = { speed: 0 }
         let randomColorArray = [0xff00ff, 0x00ff00, 0x0000ff]
 
         const parameters = {
-                color: 0xffffff,
+                color: 0xbbbbbb,
                 spin: () => {
-                    text.rotation.y += Math.PI;
+                    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
                 },
                 randomcolor: () => {
                     let randomNumber = Math.floor(Math.random() * 3)
-                    // console.log(randomNumber)
 
                     console.log(randomColorArray.indexOf(parameters.color))
 
                     material.color.set(randomColorArray[randomNumber])
-                    // console.log(randomColorArray.indexOf(0xff00ff))
-
-                    // console.log(material.color);
-                    // console.log(randomColorArray[Math.floor(Math.random() * 3)]);
                 }
-            }
+        }
             
-            panel.addColor(parameters, "color").onChange(() =>{
-                material.color.set(parameters.color)
-            }).name("Material color")
-            panel.add(parameters, "spin").name("Spin text")
-            panel.add(parameters, "randomcolor").name("Randomcolor (RGB)")
+        // Panel part
+        panel.add(material, 'wireframe', true, false).name("Material Wireframe")
+        panel.addColor(parameters, "color").onChange(() =>{
+            material.color.set(parameters.color)
+        }).name("Material color")
+        panel.add(parameters, "spin").name("Spin text")
+        panel.add(parameters, "randomcolor").name("Randomcolor (RGB)")
+        panel.add(rotationSpeed, "speed").min(0).max(100).step(0.1).name("Text Rotation Speed")
 
-        const tick = () =>
+
+        const textTick = () =>
         {
             const elapsedTime = clock.getElapsedTime() * rotationSpeed.speed
-        
+
             // Rotation
             text.rotation.y = elapsedTime
-        
+
             // Render
             renderer.render(scene, camera)
-        
+
             // Call tick again on the next frame
-            window.requestAnimationFrame(tick)
+            window.requestAnimationFrame(textTick)
         }
-        panel.add(rotationSpeed, "speed").min(0).max(100).step(0.1).name("Text Rotation speed")
         
-        tick()
+        textTick()
     }
 )
-    
-
 
 /**
  * AxesHelper
  */
-
 // const axesHelper = new THREE.AxesHelper()
 // scene.add(axesHelper)
-
-
-/**
- *  Panel
- */
-
-// panel.add(textGeometry.font)
-// panel.add(mesh.rotation.x = Math.PI * 0.25)
-
-/*** sphereRotationSpeed
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial()
-// )
-
-// scene.add(cube)
 
 /**
  * Sizes
  */
+// Fullscreen
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
+// Realtime calculs for resizing
 window.addEventListener('resize', () =>
 {
     // Update sizes
@@ -171,86 +143,49 @@ camera.position.y = 1
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
+/**
+ * Controls
+ */
+// Threejs native control
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /**
- * Donuts
+ * Sphere
  */
+const sphereNumber = 3;
+const sphereSize = 0.15;
+const sphereGeometry = new THREE.SphereBufferGeometry(sphereSize, 16 , 16);
+const sphereArray = [];
 
-// const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
-const sphereSize = 0.15
-const donutGeometry = new THREE.SphereBufferGeometry(sphereSize, 16 , 16)
-const donutArray = [];
+for(let i = 0; i < sphereNumber; i++){
+    const sphere = new THREE.Mesh(sphereGeometry, material)
 
+    sphere.position.x = (Math.random()) * 1.5
+    sphere.position.y = (Math.random()) * 1.5
+    sphere.position.z = (Math.random()) * 1.5
 
-for(let i = 0; i < 3; i++){
-    
-    const donut = new THREE.Mesh(donutGeometry, material)
+    sphereArray.push(sphere)
 
-    // donut.position.x = (Math.random() - 0.5 ) * 10
-    // donut.position.y = (Math.random() - 0.5 ) * 10
-    // donut.position.z = (Math.random() - 0.5 ) * 10
-
-    
-    // donut.rotation.x = Math.random() * Math.PI
-    // donut.rotation.y = Math.random() * Math.PI
-
-    donut.position.x = (Math.random()) * 1.5
-    donut.position.y = (Math.random()) * 1.5
-    donut.position.z = (Math.random()) * 1.5
-    
-    // const scale = Math.random()
-    // const scale = 1.5
-    // donut.scale.set(scale, scale, scale)
-
-    donutArray.push(donut)
-
-    scene.add(donut)
+    scene.add(sphere)
 }
-
-// console.log(donutArray[0]);
-// panel.add(donutArray[0].rotation, "y",).min(0).max(Math.PI * 2).step(0.01).name("Rotation")
-
-
-// const parameters = {
-//     color: 0x000000,
-//     spin: () => {
-//         gsap.to(text.rotation, { duration: 1, y: mesh.rotation.y + Math.PI })
-//     },
-//     randomcolor: () => {
-//         let randomColorArray = ["#ff00ff", "#00ff00", "#0f0f0f"]
-//         material.color.set(randomColorArray[Math.floor(Math.random() * 3)])
-//         // console.log(randomColorArray[Math.floor(Math.random() * 3)]);
-//     }
-// }
-
-// panel.addColor(parameters, "color").onChange(() =>{
-//     material.color.set(parameters.color)
-// })
-// panel.add(parameters, "spin")
-// panel.add(parameters, "randomcolor")
 
 /**
  * Renderer
  */
+// Tell webgl the canvas is canvas class="webgl" in html
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+// Fullscreen
 renderer.setSize(sizes.width, sizes.height)
+// Prevent anti-aliasing
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
- * Animate
+ * Animations
  */
 const clock = new THREE.Clock()
-let sphereRotationSpeed = 2 
-
-// donutArray.forEach(element => {
-//     console.log(element);
-// })
-
 
 const tick = () =>
 {
@@ -261,26 +196,22 @@ const tick = () =>
 
     // console.log(Math.floor((Math.random() * (100 - 1) + 1)))
 
-    // Rotation
-    // donutArray[Math.floor((Math.random() * (100 - 1) + 1))].rotation.y += 0.5
-    // donutArray[Math.floor((Math.random() * (100 - 1) + 1))].rotation.x += 0.5
-    // donutArray[Math.floor((Math.random() * (100 - 1) + 1))].rotation.z += 0.5
+    // Sphere rotate animation
+    // donutArray[Math.floor((Math.random() * (100 - 1) + 1))].rotation.y = elapsedTime * 0.5
+    // donutArray[Math.floor((Math.random() * (100 - 1) + 1))].rotation.x = elapsedTime * 0.5
+    // donutArray[Math.floor((Math.random() * (100 - 1) + 1))].rotation.z = elapsedTime * 0.5
 
-    donutArray[0].position.y = Math.sin(elapsedTime)
-    donutArray[0].position.x = Math.cos(elapsedTime)
-    donutArray[0].position.z = Math.cos(elapsedTime)
+    sphereArray[0].position.y = Math.sin(elapsedTime)
+    sphereArray[0].position.x = Math.cos(elapsedTime)
+    sphereArray[0].position.z = Math.cos(elapsedTime)
 
-    donutArray[1].position.y = Math.cos(elapsedTime)
-    donutArray[1].position.x = Math.sin(elapsedTime)
-    donutArray[1].position.z = Math.cos(elapsedTime)
+    sphereArray[1].position.y = Math.cos(elapsedTime)
+    sphereArray[1].position.x = Math.sin(elapsedTime)
+    sphereArray[1].position.z = Math.cos(elapsedTime)
 
-    donutArray[2].position.y = Math.cos(elapsedTime) 
-    donutArray[2].position.x = Math.cos(elapsedTime) 
-    donutArray[2].position.z = Math.sin(elapsedTime)
-
-
-    // Camera lookat
-    // camera.lookAt(text.scale)
+    sphereArray[2].position.y = Math.cos(elapsedTime) 
+    sphereArray[2].position.x = Math.cos(elapsedTime) 
+    sphereArray[2].position.z = Math.sin(elapsedTime)
 
     // Render
     renderer.render(scene, camera)
